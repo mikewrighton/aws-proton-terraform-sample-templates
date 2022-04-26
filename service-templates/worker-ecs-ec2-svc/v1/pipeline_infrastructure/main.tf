08acdb7de11f0bd9f26d7a87d6fd77e62c4af6b0
Mike Wrighton <mikewr@amazon.com>
2022-04-26 12:10:07 -0400

    Add privaledged mode.

--- service-templates/worker-ecs-ec2-svc/v1/pipeline_infrastructure/main.tf
+++ service-templates/worker-ecs-ec2-svc/v1/pipeline_infrastructure/main.tf
@@ -37,6 +37,7 @@ resource "aws_codebuild_project" "build_project" {
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
 
     environment_variable {
       name  = "repo_name"
