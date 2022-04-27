a70adc14c314fcc95fc84e6c9400da7ddd21cd3f
Mike Wrighton <mikewr@amazon.com>
2022-04-27 12:55:05 -0400

    WIP worker-lambda-svc

--- service-templates/worker-lambda-svc/v1/instance_infrastructure/data.tf
+++ service-templates/worker-lambda-svc/v1/instance_infrastructure/data.tf
@@ -12,24 +12,22 @@ data "aws_iam_policy_document" "ecs_processing_queue_policy_document" {
       "sqs:DeleteMessage"
     ]
     principals {
-      identifiers = [aws_iam_role.ecs_processing_queue_task_def_task_role.arn]
-      type        = "AWS"
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
     }
     resources = [
-      aws_sqs_queue.ecs_processing_queue.arn
+      aws_sqs_queue.processing_queue.arn
     ]
   }
   statement {
     effect = "Allow"
-    actions = [
-      "sqs:SendMessage",
-    ]
+    actions = ["sqs:SendMessage"]
     principals {
       identifiers = ["sns.amazonaws.com"]
       type        = "Service"
     }
     resources = [
-      aws_sqs_queue.ecs_processing_queue.arn
+      aws_sqs_queue.processing_queue.arn
     ]
 
     condition {
@@ -53,5 +51,27 @@ data "aws_iam_policy_document" "ecs_processing_queue_task_def_task_role_policy_d
     resources = [
       aws_sqs_queue.ecs_processing_queue.arn
     ]
+  }
+}
+
+data "archive_file" "lambda_zip_inline" {
+  type        = "zip"
+  output_path = "lambda_zip_inline.zip"
+
+  source {
+    filename = "index.js"
+    content  = <<EOF
+        exports.handler = async (event, context) => {
+          try {
+            // Log event and context object to CloudWatch Logs
+            console.log("Event: ", JSON.stringify(event, null, 2));
+            console.log("Context: ", JSON.stringify(context, null, 2));
+            return {};
+          } catch (error) {
+            console.error(error);
+            throw new Error(error);
+          }
+        };
+EOF
   }
 }
